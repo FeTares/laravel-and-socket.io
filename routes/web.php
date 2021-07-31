99a8e85cf0cbf100ce2cc75dfda38a0f658dca99
Felipe Tavares <felipe.amtavares1@gmail.com>
2021-07-31 12:06:40 -0300

    Create Event and Listeners and Observer

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Events\PostCreated;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
@@ -18,10 +19,12 @@ use Illuminate\Support\Str;
 Route::get('/create-post', function () {
     $user = User::first();
 
-    $user->post()->create([
+    $post = $user->post()->create([
         'title' => Str::random(150),
         'body' => Str::random(400),
     ]);
+
+    // event(new PostCreated($post));
 
     return 'ok';
 });
